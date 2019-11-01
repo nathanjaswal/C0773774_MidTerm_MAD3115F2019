9bf00f54644eae3903464d7a57d8bd7367c51781
nathanjaswal <nathanjaswal@gmail.com>
2019-11-01 13:13:50 -0400

    [Nitin] customer code added

--- C0773774_MidTerm_MAD3115F2019/Controller/Home/CustomerList/CustomerListTVC.swift
+++ C0773774_MidTerm_MAD3115F2019/Controller/Home/CustomerList/CustomerListTVC.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class CustomerListTVC: UITableViewCell {
-
+    
+    // MARK: - Properties
+    @IBOutlet var contan_view: UIView!
+    @IBOutlet var name_lbl: UILabel!
+    
+    // MARK: - Life Cycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
--- C0773774_MidTerm_MAD3115F2019/Controller/Home/CustomerList/CustomerListVC.swift
+++ C0773774_MidTerm_MAD3115F2019/Controller/Home/CustomerList/CustomerListVC.swift
@@ -28,8 +28,26 @@ class CustomerListVC: UIViewController {
     
     // MARK:- Action
     @IBAction func moreBtnClicked(_ sender: Any) {
-       //
-        self.navigationController?.popToRootViewController(animated: true)
+        //
+        let alertController = UIAlertController(title: "Nitin Jaswal", message: "Do You want to Sign Out?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Yes", style: UIAlertAction.Style.destructive) {
+            UIAlertAction in
+            
+            //
+            self.navigationController?.popToRootViewController(animated: true)
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: UIAlertAction.Style.cancel) {
+            UIAlertAction in
+        }
+        
+        // Add the actions
+        alertController.addAction(okAction)
+        alertController.addAction(cancelAction)
+        
+        // Present the controller
+        self.present(alertController, animated: true, completion: nil)
+       
     }
     
     @IBAction func addBtnClicked(_ sender: Any) {
@@ -52,6 +70,49 @@ class CustomerListVC: UIViewController {
         
     }
     
-   
+}
 
+// MARK:- UITableViewDataSource
+extension CustomerListVC: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return countryArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = cTableView.dequeueReusableCell(withIdentifier: "CGCountryTableViewCell", for: indexPath ) as! CGCountryTableViewCell
+        //
+        let country = countryArr[indexPath.row] as? String ?? ""
+        cell.title_lbl.text = country
+        
+        //
+        if(selecCountry == country) {
+            cell.accessoryType = .checkmark
+        }else {
+            cell.accessoryType = .none
+        }
+        
+        return cell
+    }
+    
+}
+
+// MARK:- UITableViewDelegate
+extension CustomerListVC: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        
+        return 70
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //
+        tableView.deselectRowAtIndexPath(index, animated: true)
+        
+        //
+        
+        
+    }
 }
