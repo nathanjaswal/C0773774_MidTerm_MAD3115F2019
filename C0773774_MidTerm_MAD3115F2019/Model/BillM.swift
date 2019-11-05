b6a250c77584c82a295ffbacc7b10696fcdb9b80
nathanjaswal <nathanjaswal@gmail.com>
2019-11-05 16:05:22 -0500

    [Nitin] bill model updated

--- C0773774_MidTerm_MAD3115F2019/Model/BillM.swift
+++ C0773774_MidTerm_MAD3115F2019/Model/BillM.swift
@@ -8,4 +8,51 @@
 
 import Foundation
 
+struct BillM: Codable {
+    
+    // MARK: - Properties
+    var internet: InternetM?
+    var mobile: MobileM?
+    var hydro: HydroM?
+    
+    //var bill: [NSDictionary]//
+    
+}
 
+struct InternetM: Codable {
+    
+    // MARK: - Properties
+    var id: String?
+    var date: String?
+    var type: String?
+    var amount: Float?
+    var name: String?
+    var data: Float?
+}
+
+struct MobileM: Codable {
+    
+    // MARK: - Properties
+    var id: String?
+    var date: String?
+    var type: String?
+    var amount: Float?
+    var name: String?
+    var plan: String?
+    var phoneNo: String?
+    var data: Float?
+    var minutes: String?
+    
+}
+
+struct HydroM: Codable {
+    
+    // MARK: - Properties
+    var id: String?
+    var date: String?
+    var type: String?
+    var amount: Float?
+    var name: String?
+    var units: Float?
+
+}
